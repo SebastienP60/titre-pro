67f3ebd0582b71c8d6a2736f55b28d7c22d78b86
Sébastien Pilatte <seb.seb60@hotmail.fr>
2020-09-23 15:27:56 +0200

    modif

--- armes-de-poing.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../header.php';
-?>
-<body>
-    <h1>Les répliques de poing</h1>
-</body>
-<?php
-include '../footer.php';
-?>
--- armes-longues.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'header.php';
-?>
-<body>
-    <h1>Les répliques longues</h1>
-</body>
-<?php
-include 'footer.php';
-?>
